d9c4287c18015d27cff040795c9d200dd6dda561
System Administrator <root@Rajkumars-MacBook-Pro.local>
2018-04-26 12:49:13 -0500

    Programns

---
+++ DisplayDayOfWeek.java
@@ -0,0 +1,41 @@
+/*
+  Display Day of Week using Java Calendar
+  This example shows how to display day of week like Monday, Tuesday etc.  
+  using Java Calendar class.
+*/
+ 
+import java.util.Calendar;
+ 
+public class DisplayDayOfWeek {
+ 
+  public static void main(String[] args) {
+ 
+    //create Calendar instance
+    Calendar now = Calendar.getInstance();
+    
+    System.out.println("Current date : " + (now.get(Calendar.MONTH) + 1)
+                        + "-"
+                        + now.get(Calendar.DATE)
+                        + "-"
+                        + now.get(Calendar.YEAR));
+    
+    //create an array of days
+    String[] strDays = new String[]{
+                      "Sunday",
+                      "Monday", 
+                      "Tuesday",
+                      "Wednesday",
+                      "Thusday",
+                      "Friday",
+                      "Saturday"
+                    };
+    
+    //Day_OF_WEEK starts from 1 while array index starts from 0      
+    System.out.println("Current day is : " + 
+              strDays[now.get(Calendar.DAY_OF_WEEK) - 1]
+              );
+    
+  
+  }
+  
+}
